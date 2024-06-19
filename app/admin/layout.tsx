737437587f55e88b6ede96ec17499d4d7543aecb
Mohamed Elsayed <122582599+MoElsayed90@users.noreply.github.com>
2024-06-19 08:17:29 +0300

    update handle Auth route and Layout for admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,8 +1,28 @@
-const layout = () => {
+"use client"
+
+import Login from "@/components/admin-panel/Login";
+import Loader from "@/components/admin-panel/loader";
+import { useAppSelector } from "@/redux/hooks";
+import { useSession } from "next-auth/react";
+import { ReactNode } from "react";
+
+const Layout = ({children}:{children:ReactNode}) => {
+  const isLoading = useAppSelector(store => store.LoadingReducer)
+  const {data:session}=useSession()
+  if(!session?.user){
+    return <Login/>;
+  }
   return (
-    <div>layout</div>
+    <div className="flex">
+    {/* <SideBar/> */}
+    <div className="w-full h-full">
+      {/* <Navbar/> */}
+      <div className="bg-gray-200 p-4 h-[calc(100vh-64px)]">{children}</div>
+    </div>
+    {isLoading && <Loader/>}
+    </div>
   )
 
 }
 
-export default layout;
+export default Layout;
