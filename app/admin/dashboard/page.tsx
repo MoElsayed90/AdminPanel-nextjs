139be73e7627475e43bea6b0f446ae71a7275047
Mohamed Elsayed <122582599+MoElsayed90@users.noreply.github.com>
2024-06-19 09:54:26 +0300

    update : add interface IProducts

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,4 +1,31 @@
+"use client"
+
+import { setLoading } from "@/redux/features/loadingSlice";
+import { useAppDispatch } from "@/redux/hooks";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export interface IProduct {
+  _id: string;
+  imgSrc: string;
+  fileKey: string;
+  name: string;
+  price: string;
+  category: string;
+}
 const Dashboard = () => {
+  const [product, setProducts] = useState([]);
+  const [openPopup, setOpenPopup] = useState(false);
+  const [updateTable, setUpdateTable] = useState(false);
+  const dispatch = useAppDispatch()
+  useEffect(()=>{
+    dispatch(setLoading(true))
+    axios.get("api/get_products")
+    .then((res)=> setProducts(res.data))
+    .catch(error=>console.log(error))
+    .finally(()=>dispatch(setLoading(false)))
+  }
+    ,[updateTable])
   return (
     <div>Dashboard</div>
   )
